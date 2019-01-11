511de9177362fe3e3e01eef264ae63ded59362c0
Arvid E. Picciani <aep@exys.org>
2019-01-11 10:28:57 +0100

    m

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,23 @@
-extern crate regex;
-extern crate failure;
-
 use std::io::BufReader;
 use std::io::BufRead;
 use std::io::Read;
-use regex::Regex;
 use std::collections::HashMap;
-use failure::Error;
 
-pub fn parse_mtd<R: Read> (r: R) -> Result<HashMap<String, u32>, Error> {
+pub fn parse_mtd<R: Read> (r: R) -> Result<HashMap<String, String>, std::io::Error> {
     let mut ret = HashMap::new();
-    let re = Regex::new(r#"^mtd([0-9]*):[^"]*"([^"]*)""#)?;
     let reader = BufReader::new(r);
     for line in  reader.lines().skip(1) {
         if let Ok(line) = line {
-            for cap in re.captures_iter(&line) {
-                ret.insert(cap[2].to_string(), cap[1].parse::<u32>().unwrap());
+            let line : Vec<&str> = line.split_whitespace().collect();
+            if line.len() < 4 {
+                continue;
             }
+            let mtd = match line[0].trim().split(":").next() {
+                Some(v) => v,
+                None => continue,
+            }.to_string();
+            let name = line[3].trim().replace("\"","").to_string();
+            ret.insert(name, mtd);
         }
     }
     Ok(ret)
@@ -45,7 +46,8 @@ mtd15: 00000400 00010000 "identity"
 mtd16: 00010000 00010000 "art"
 "#;
 let r = parse_mtd(parts as &[u8]).unwrap();
-assert!(r["device-id"] == 8);
-assert!(r["identity"] == 15);
-assert!(r["art"] == 16);
+assert!(r["device-id"] == "mtd8");
+assert!(r["identity"] == "mtd15");
+assert!(r["art"] == "mtd16");
+assert!(r.get("mtd13") == None);
 }
